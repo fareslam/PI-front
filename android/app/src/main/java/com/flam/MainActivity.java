fb864e50f4596c3da064dc1b30eaa3fc19beaed6
fareslam <fareslaamloum@gmail.com>
2023-05-06 11:12:37 +0000

    flam

---
+++ android/app/src/main/java/com/flam/MainActivity.java
@@ -0,0 +1,5 @@
+package com.flam;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flam',
+  appName: 'material-dashboard-angular',
+  webDir:'dist/material-dashboard-angular',bundledWebRuntime:false
+};
+
+export default config;
